be17a25357760388d2e76ac297488cc1ebe83b57
Otavio Andrade <otavio.sto@gmail.com>
2026-08-16 20:18:29 -0300

    feat: consumer record

--- apps/fraud-engine/src/main/java/br/com/frauddetection/engine/consumer/TransactionConsumer.java
+++ apps/fraud-engine/src/main/java/br/com/frauddetection/engine/consumer/TransactionConsumer.java
@@ -1,5 +1,6 @@
 package br.com.frauddetection.engine.consumer;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -9,7 +10,12 @@ public class TransactionConsumer {
             topics = "transactions.v1",
             groupId = "fraud-engine"
     )
-    public void consumer(String message){
-        System.out.println("Mensagem recebida: " + message);
+    public void listener(ConsumerRecord<String, String> record){
+        System.out.println("Tópico: " + record.topic());
+        System.out.println("Partição: " + record.partition());
+        System.out.println("Offset: " + record.offset());
+        System.out.println("Chave: " + record.key());
+        System.out.println("Mensagem: " + record.value());
+        System.out.println(record.headers());
     }
 }
