5ee3c418d8f865ccff2852f22bf2364c2d5b4853
Otavio Andrade <otavio.sto@gmail.com>
2026-08-16 18:56:22 -0300

    feat: criar cluster simplificado e aplicação spring inicial

---
+++ apps/fraud-engine/src/test/java/br/com/frauddetection/engine/FraudEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.frauddetection.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
