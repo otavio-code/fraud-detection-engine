2571d40b53495a16b1c32e08c0fd2b491b819842
Otavio Andrade <otavio.sto@gmail.com>
2026-08-17 23:54:13 -0300

    [conf] - configurar retry e simular retry

---
+++ apps/fraud-engine/src/main/java/br/com/frauddetection/engine/config/KafkaConsumerConfig.java
@@ -0,0 +1,7 @@
+package br.com.frauddetection.engine.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaConsumerConfig {
+}
--- apps/fraud-engine/src/main/java/br/com/frauddetection/engine/consumer/TransactionConsumer.java
+++ apps/fraud-engine/src/main/java/br/com/frauddetection/engine/consumer/TransactionConsumer.java
@@ -8,6 +8,7 @@ import java.time.Instant;
 
 @Component
 public class TransactionConsumer {
+/*
     @KafkaListener(
             topics = "transactions.v1",
             groupId = "fraud-engine"
@@ -22,4 +23,24 @@ public class TransactionConsumer {
         Instant instant = Instant.ofEpochMilli(record.timestamp());
         System.out.println(instant);
     }
+ */
+@KafkaListener(
+        topics = "transactions.v1",
+        groupId = "fraud-engine"
+)
+public void consume(ConsumerRecord<String, String> record) {
+
+    System.out.println("Tópico: " + record.topic());
+    System.out.println("Partição: " + record.partition());
+    System.out.println("Offset: " + record.offset());
+    System.out.println("Chave: " + record.key());
+    System.out.println("Mensagem: " + record.value());
+
+    if (record.value().contains("\"id_transacao\":\"tx-fail\"")) {
+        System.out.println("Simulando falha no processamento...");
+        throw new RuntimeException("Falha proposital");
+    }
+
+    System.out.println("Processamento concluído");
+}
 }
