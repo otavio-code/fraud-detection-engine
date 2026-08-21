d727d2121d6a2044d1c1ed0285247088bd6a9749
Otavio Andrade <otavio.sto@gmail.com>
2026-08-20 21:17:34 -0300

    conf: criar o uma lib para contrato

--- apps/fraud-engine/src/main/java/br/com/frauddetection/engine/consumer/TransactionConsumer.java
+++ apps/fraud-engine/src/main/java/br/com/frauddetection/engine/consumer/TransactionConsumer.java
@@ -4,8 +4,6 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-import java.time.Instant;
-
 @Component
 public class TransactionConsumer {
 /*
