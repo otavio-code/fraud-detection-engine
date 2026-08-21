0814e35bb3e72ce061abc9374cbd4ab67c1ea362
Otavio Andrade <otavio.sto@gmail.com>
2026-08-20 22:42:41 -0300

    conf: criar producer

--- apps/simulator/src/main/java/br/com/frauddetection/simulator/producer/TransactionProducer.java
+++ apps/simulator/src/main/java/br/com/frauddetection/simulator/producer/TransactionProducer.java
@@ -1,4 +1,22 @@
 package br.com.frauddetection.simulator.producer;
 
+import br.com.frauddetection.events.TransactionEvent;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class TransactionProducer {
+
+    private static final String TOPIC = "transactions.v1";
+    private final KafkaTemplate<String, TransactionEvent> kafkaTemplate;
+
+    public TransactionProducer(
+            KafkaTemplate<String, TransactionEvent> kafkaTemplate
+    ) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void send(String key, TransactionEvent event) {
+        kafkaTemplate.send(TOPIC, key, event);
+    }
 }
