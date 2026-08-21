cabed596a9f97a5cc0c6299839f4f771fc8233e8
Otavio Andrade <otavio.sto@gmail.com>
2026-08-20 22:36:08 -0300

    feat: criando aplicação simulator

---
+++ apps/simulator/src/main/java/br/com/frauddetection/simulator/SimulatorApplication.java
@@ -0,0 +1,13 @@
+package br.com.frauddetection.simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimulatorApplication.class, args);
+    }
+
+}
---
+++ apps/simulator/src/test/java/br/com/frauddetection/simulator/SimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.frauddetection.simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
