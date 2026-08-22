a1de329ab9b53bb2655a1f502734cfe8a9d102c2
Otavio Andrade <otavio.sto@gmail.com>
2026-08-22 19:41:32 -0300

    test: teste unitário da regra de limite acima criado

--- apps/fraud-engine/src/test/java/br/com/frauddetection/engine/rules/HighValueTransactionRuleTest.java
+++ apps/fraud-engine/src/test/java/br/com/frauddetection/engine/rules/HighValueTransactionRuleTest.java
@@ -5,11 +5,11 @@ import br.com.frauddetection.events.TransactionEvent;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import javax.swing.*;
 
 import java.math.BigDecimal;
 import java.time.Instant;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 class HighValueTransactionRuleTest {
@@ -25,6 +25,7 @@ class HighValueTransactionRuleTest {
         HighValueTransactionRule rule = new HighValueTransactionRule(
                 configuracaoRegraService
         );
+
         TransactionEvent event = TransactionEvent.newBuilder()
                 .setIdEvento("123456789")
                 .setIdTransacao("456")
@@ -37,8 +38,107 @@ class HighValueTransactionRuleTest {
                 .setDataHoraTransacao(Instant.now())
                 .build();
 
+        // Act
+        FraudRuleResult result = rule.evaluate(event);
 
+        // Assert
+        assertTrue(
+                result.suspeita(),
+                "A transação deveria ser considerada suspeita"
+        );
 
-        // executar a regra
+        assertEquals(
+                "TRANSACAO_VALOR_ALTO",
+                result.regra()
+        );
+
+        assertEquals(
+                "Transação acima do limite de R$ 10000.00",
+                result.motivo()
+        );
+    }
+
+    @Test
+    void deveConsiderarTransacaoAbaixoDoLimiteComoNaoSuspeita(){
+        // Arrange
+        ConfiguracaoRegraService configuracaoRegraService = Mockito.mock(ConfiguracaoRegraService.class);
+        when(configuracaoRegraService.obterLimiteTransacaoValorAlto()
+        ).thenReturn(
+                new BigDecimal("10000.00")
+        );
+
+        HighValueTransactionRule rule = new HighValueTransactionRule(
+                configuracaoRegraService
+        );
+
+        TransactionEvent event = TransactionEvent.newBuilder()
+                .setIdEvento("123456789")
+                .setIdTransacao("456")
+                .setIdCliente("abc")
+                .setIdContaOrigem("02646")
+                .setIdContaDestino("02548")
+                .setValorTransacao(new BigDecimal("5000.00"))
+                .setCodigoMoeda("BRL")
+                .setTipoTransacao("PIX")
+                .setDataHoraTransacao(Instant.now())
+                .build();
+
+        // Act
+        FraudRuleResult result = rule.evaluate(event);
+
+        // Assert
+        assertFalse(
+                result.suspeita(),
+                "A transação não deveria ser considerada suspeita"
+        );
+
+        assertEquals(
+                "TRANSACAO_VALOR_ALTO",
+                result.regra()
+        );
+
+        assertNull(result.motivo());
+    }
+
+    @Test
+    void deveConsiderarTransacaoAbaixoDoLimiteComoNaoSuspeitaSeValorIgualLimite(){
+        // Arrange
+        ConfiguracaoRegraService configuracaoRegraService = Mockito.mock(ConfiguracaoRegraService.class);
+        when(configuracaoRegraService.obterLimiteTransacaoValorAlto()
+        ).thenReturn(
+                new BigDecimal("10000.00")
+        );
+
+        HighValueTransactionRule rule = new HighValueTransactionRule(
+                configuracaoRegraService
+        );
+
+        TransactionEvent event = TransactionEvent.newBuilder()
+                .setIdEvento("123456789")
+                .setIdTransacao("456")
+                .setIdCliente("abc")
+                .setIdContaOrigem("02646")
+                .setIdContaDestino("02548")
+                .setValorTransacao(new BigDecimal("10000.00"))
+                .setCodigoMoeda("BRL")
+                .setTipoTransacao("PIX")
+                .setDataHoraTransacao(Instant.now())
+                .build();
+
+        // Act
+        FraudRuleResult result = rule.evaluate(event);
+
+        // Assert
+        assertFalse(
+                result.suspeita(),
+                "A transação não deveria ser considerada suspeita"
+        );
+
+        assertEquals(
+                "TRANSACAO_VALOR_ALTO",
+                result.regra()
+        );
+
+        assertNull(result.motivo());
     }
 }
