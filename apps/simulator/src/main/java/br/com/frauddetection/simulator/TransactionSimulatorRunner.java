630b202189a90c58765fba15d6629b4e8a145faa
Otavio Andrade <otavio.sto@gmail.com>
2026-08-23 19:48:44 -0300

    feat: nova regra adicionada, alterando simulator para enviar várias mensagens

--- apps/simulator/src/main/java/br/com/frauddetection/simulator/TransactionSimulatorRunner.java
+++ apps/simulator/src/main/java/br/com/frauddetection/simulator/TransactionSimulatorRunner.java
@@ -19,18 +19,29 @@ public class TransactionSimulatorRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        TransactionEvent event = TransactionEvent.newBuilder()
-                .setIdEvento("98798791440")
-                .setIdTransacao("123456789")
-                .setIdCliente("001")
-                .setIdContaOrigem("123")
-                .setIdContaDestino("456")
-                .setValorTransacao(new BigDecimal("15000.00"))
-                .setCodigoMoeda("BRL")
-                .setTipoTransacao("PIX")
-                .setDataHoraTransacao(Instant.now())
-                .build();
 
-        transactionProducer.send("123", event);
+        for (int i = 1; i <= 6; i++) {
+
+            TransactionEvent event = TransactionEvent.newBuilder()
+                    .setIdEvento("evento-velocidade-" + i)
+                    .setIdTransacao("transacao-velocidade-" + i)
+                    .setIdCliente("cliente-001")
+                    .setIdContaOrigem("123")
+                    .setIdContaDestino("456")
+                    .setValorTransacao(new BigDecimal("100.00"))
+                    .setCodigoMoeda("BRL")
+                    .setTipoTransacao("PIX")
+                    .setDataHoraTransacao(Instant.now())
+                    .build();
+
+            transactionProducer.send(
+                    event.getIdContaOrigem(),
+                    event
+            );
+
+            System.out.println(
+                    "Evento publicado: " + event.getIdEvento()
+            );
+        }
     }
 }
