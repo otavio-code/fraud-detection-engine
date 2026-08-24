8c53235471e37212990eebff9620cb82aae44d86
Otavio Andrade <otavio.sto@gmail.com>
2026-08-24 07:47:42 -0300

    feat: criação do alerta e atualização do dockercompose para criação de tópicos

---
+++ apps/fraud-engine/src/main/java/br/com/frauddetection/engine/alert/AlertKafkaProducerConfig.java
@@ -0,0 +1,61 @@
+package br.com.frauddetection.engine.config;
+
+import br.com.frauddetection.engine.alert.FraudAlert;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class AlertKafkaProducerConfig {
+
+    @Bean
+    public ProducerFactory<String, FraudAlert>
+    alertProducerFactory(
+            @Value("${spring.kafka.bootstrap-servers}")
+            String bootstrapServers
+    ) {
+
+        Map<String, Object> properties =
+                new HashMap<>();
+
+        properties.put(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                bootstrapServers
+        );
+
+        properties.put(
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class
+        );
+
+        properties.put(
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                JsonSerializer.class
+        );
+
+        return new DefaultKafkaProducerFactory<>(
+                properties
+        );
+    }
+
+    @Bean
+    public KafkaTemplate<String, FraudAlert>
+    alertKafkaTemplate(
+            ProducerFactory<String, FraudAlert>
+                    alertProducerFactory
+    ) {
+
+        return new KafkaTemplate<>(
+                alertProducerFactory
+        );
+    }
+}
